2cbcf2ce2d53f4fa7447f1403434709f527fc80e
gleonag <gleonag@gmail.com>
2022-10-08 11:02:40 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+
+let euro = 1;
+let dollarEuro = 1.2;
+let yenEuro = 127.9;
+let libraEuro = 0.8;
+
+fromDollartoYen = () => {
+    let dolarValueFromEuro = euro / dollarEuro;
+    return dolarValueFromEuro * yenEuro;
+}
+
+from
